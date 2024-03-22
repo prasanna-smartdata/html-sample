e4276afa3c8dc351cbb4401b4c2a84d60e033d13
bathapudi <prasanna.bethapudi@smartdataglobal.in>
2024-03-22 12:59:01 +0530

    test

--- js/products.js
+++ js/products.js
@@ -53,10 +53,24 @@ document.addEventListener("DOMContentLoaded", function() {
         // Check if the div exists
         if (div) {
             getProducts().then((data)=>
-                         {
-                            const text= JSON.stringify(data, null, 2);
-                            div.innerHTML = text;
-                        });
+                {
+                    const setOfFeatures = new Set()
+                    const sortedProducts = data.sort(
+                        (a, b) => a.prices.recurringFee - b.prices.recurringFee,
+                    );
+                    const products= JSON.stringify(sortedProducts, null, 2);
+                   
+                    data.map(obj=>obj.features).forEach(element => {
+                        element.forEach(text=>setOfFeatures.add('<div class="table-cell-2">'+ text +'</div>'))
+                    });
+                    // Convert Set to an array using spread operator
+                    const setToArray = [...setOfFeatures];
+
+                    // Convert array to string
+                    const setToString = setToArray.join('');      
+                    console.log(setToString) 
+                    div.innerHTML = createHtml(setToString, products);
+                });
             // Add content to the div
             div.innerHTML = "New content added to the div!";
         } else {
@@ -67,3 +81,14 @@ document.addEventListener("DOMContentLoaded", function() {
     // Call the function to add content on page load
     addContentToDiv();
 });
+
+function createHtml(features,data){
+    var html="<div class='pricing-table-wrapper'> <div class='w-layout-grid pricing-2'>";
+    var featuresHtml='<div class="table-header-2"/> <div class="table-cell-2">';
+    featuresHtml+='<div class="price-title">Subscription</div>';
+    featuresHtml+='<div class="w-embed">  <style> html.w-mod-js *[data-ix="tooltip-hover"], .tooltip-trigger { display: flex !important; }</style>  </div></div>';
+    featuresHtml+=features;
+    html+=featuresHtml;
+    var endHtml=html+ "</div></div>";
+    return endHtml;
+}
