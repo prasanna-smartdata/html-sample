73614695d1c68a9c9c8568336c46a8698580824d
bathapudi <prasanna.bethapudi@smartdataglobal.in>
2024-03-23 20:38:51 +0530

    test

--- js/products.js
+++ js/products.js
@@ -77,7 +77,7 @@ function createHtml(features,products,commonFeatures){
     var html="<div class='pricing-table-wrapper'> <div class='pricing-2'>";
     // var featuresHtml='<div class="table-cell-2">';
     // featuresHtml+='<div class="table-header-2">All Pricing Includes</div>';
-    // featuresHtml+='<div class="w-embed">  <style> html.w-mod-js *[data-ix="tooltip-hover"], .tooltip-trigger { display: flex !important; }</style>  </div></div>';
+    // featuresHtml+='<div class="body-text-2 m">  <style> html.w-mod-js *[data-ix="tooltip-hover"], .tooltip-trigger { display: flex !important; }</style>  </div></div>';
     // featuresHtml+=features;
     html+=products.map(product=>createPlanHtml(product,commonFeatures)).join('');
    // html+=featuresHtml;
@@ -90,10 +90,10 @@ function createPlanHtml(product,commonFeatures){
     const features=getValuesNotInArray(product.features,commonFeatures);
     var html="<div class='table-header-2'>";
     html+='<div class="table-cell-2">'+product.name+'</div>';
-    html+='<div>'+`Setup Fee - ${product.prices.setupFee}`+'</div>';
-    html+='<div>'+`${product.prices.overage.limit} rides / month`+'</div>';
-    html+='<div>'+`Then ${product.prices.overage.fee} / ride`+'</div>';   
-    const featureString=features.map(feature=>'<div>'+ feature+'</div>')
+    html+='<div class="body-text-2 m">'+`Setup Fee - ${product.prices.setupFee}`+'</div>';
+    html+='<div class="body-text-2 m">'+`${product.prices.overage.limit} rides / month`+'</div>';
+    html+='<div class="body-text-2 m">'+`Then ${product.prices.overage.fee} / ride`+'</div>';   
+    const featureString=features.map(feature=>'<div class="body-text-2 m">'+ feature+'</div>')
     html+=featureString.join('');
     
     html+='</div>';
