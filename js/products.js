b425b319c1f55e271becd7a32accb102f521fcf5
bathapudi <prasanna.bethapudi@smartdataglobal.in>
2024-03-25 11:36:36 +0530

    test

--- js/products.js
+++ js/products.js
@@ -1,5 +1,5 @@
-// $(document).ready(function() {
-//     // URL of the API you want to fetch
+
+    // URL of the API you want to fetch
     const apiUrl =  'https://api.evertransit.whipclient.io/internal-api/v1/products';
     const username = 'et_signup_client';
     const password = 'TejmcySXPJix2MvaF1k3E0';
@@ -66,6 +66,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Call the function to add content on page load
     addContentToDiv();
+
+    
 });
 function findCommonFeatures(arrays) {
     return arrays.reduce((accumulator, currentValue) => {
@@ -88,15 +90,20 @@ function createHtml(features,products,commonFeatures){
 
 function createPlanHtml(product,commonFeatures){
     const features=getValuesNotInArray(product.features,commonFeatures);
-    var html="<div class='div-block-4'><div class='table-header-2'>";
-    html+='<div class="table-cell-2">'+product.name+'</div></div>';
-    html+='<div class="body-text-2 m">'+`Setup Fee - ${product.prices.setupFee}`+'</div>';
-    html+='<div class="body-text-2 m">'+`${product.prices.overage.limit} rides / month`+'</div>';
-    html+='<div class="body-text-2 m">'+`Then ${product.prices.overage.fee} / ride`+'</div>';   
-    const featureString=features.map(feature=>'<div class="body-text-2 m">'+ feature+'</div>')
-    html+=featureString.join('');
+    var html="<div class='cHQdJ'><label class='sc-gwZsXD gcIrqH'><div class='jlhVkr'>";
+    html+='<span class="hemwwv">'+product.name+'</span>';
+    html+='<span><span class="sc-eIVEXM dyJMbD">$'+product.prices.setupFee+'<span class="sc-RpuvT ebTNMh"> / Month</span></span>';
+    html+='<li class="sc-iipuKH dHKZej"><svg class="sc-bPzAnn flUzcH" fill="currentColor" width="14px" height="10px" viewBox="0 0 14 10" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><title></title><g id="Variant-3" stroke="none" stroke-width="1" fill-rule="evenodd"><path d="M13.6506722,2.00848715 L5.90623037,9.6551278 C5.67389058,9.88453999 5.36890372,10 5.0639168,10 C4.75892988,10 4.45394301,9.88453999 4.22160323,9.6551278 L0.349491416,5.83180748 C-0.116497139,5.37190617 -0.116497139,4.62830919 0.349491416,4.16840789 C0.81526177,3.70829114 1.56813,3.70829114 2.03411855,4.16840789 L5.0639168,7.16002843 L11.9660451,0.345087565 C12.4318154,-0.115029188 13.1846836,-0.115029188 13.6506722,0.345087565 C14.1164426,0.804988866 14.1164426,1.54837039 13.6506722,2.00848715 Z" id="checkmark"></path></g></svg><span class="sc-nUItV fJlsIf"><b class="sc-gsxalj ePltAh">'+`${product.prices.overage.limit} rides / month`+'</span></li>';
+    html+='<li class="sc-iipuKH dHKZej"><svg class="sc-bPzAnn flUzcH" fill="currentColor" width="14px" height="10px" viewBox="0 0 14 10" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><title></title><g id="Variant-3" stroke="none" stroke-width="1" fill-rule="evenodd"><path d="M13.6506722,2.00848715 L5.90623037,9.6551278 C5.67389058,9.88453999 5.36890372,10 5.0639168,10 C4.75892988,10 4.45394301,9.88453999 4.22160323,9.6551278 L0.349491416,5.83180748 C-0.116497139,5.37190617 -0.116497139,4.62830919 0.349491416,4.16840789 C0.81526177,3.70829114 1.56813,3.70829114 2.03411855,4.16840789 L5.0639168,7.16002843 L11.9660451,0.345087565 C12.4318154,-0.115029188 13.1846836,-0.115029188 13.6506722,0.345087565 C14.1164426,0.804988866 14.1164426,1.54837039 13.6506722,2.00848715 Z" id="checkmark"></path></g></svg><span class="sc-nUItV fJlsIf"><b class="sc-gsxalj ePltAh">'+`Then ${product.prices.overage.fee} / ride`+'</span></li>';
     
-    html+='</div>';
+    html+='</span>'
+    // html+='<div class="body-text-2 m">'+`Setup Fee - ${product.prices.setupFee}`+'</div>';
+    // html+='<div class="body-text-2 m">'+`${product.prices.overage.limit} rides / month`+'</div>';
+    // html+='<div class="body-text-2 m">'+`Then ${product.prices.overage.fee} / ride`+'</div>';   
+    // const featureString=features.map(feature=>'<div class="body-text-2 m">'+ feature+'</div>')
+    // html+=featureString.join('');
+    
+    html+='</label></div>';
     return html;
 
 }
@@ -104,3 +111,14 @@ function createPlanHtml(product,commonFeatures){
 function getValuesNotInArray(arr1, arr2) {
     return arr1.filter(item => arr2.indexOf(item) === -1);
 }
+
+function loadCSS(filename) {
+    var link = document.createElement("link");
+    link.href = filename;
+    link.rel = "stylesheet";
+    link.type = "text/css";
+    document.head.appendChild(link);
+}
+
+// Example usage:
+loadCSS("css/styles.css");
