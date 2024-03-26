a094f1838d1a74d2ca4a34b6d050d233d52d3805
bathapudi <prasanna.bethapudi@smartdataglobal.in>
2024-03-26 17:14:37 +0530

    Test

--- js/products.js
+++ js/products.js
@@ -74,9 +74,7 @@ function findCommonFeatures(arrays) {
 }
 function createHtml(features,products,commonFeatures){
     var html="<div class='pricing-table-wrapper flex-container'><div class= 'pricing-2'>";
-    var featuresHtml='<div class="table-cell-2">';
-    featuresHtml+='<div class="table-header-2">All Pricing Includes</div>';
-    featuresHtml+='<div class="body-text-2 m">  <style> html.w-mod-js *[data-ix="tooltip-hover"], .tooltip-trigger { display: flex !important; }</style>  </div></div>';
+    var featuresHtml='<div class="table-header-2">All Pricing Includes</div>';
     featuresHtml+=features+"</div>";
     html+=featuresHtml;
     html+= '<div class="card-game__cards"><button class="left scrollbutton" onclick="leftScroll()">&lt;</button><ul class="card-game__cards-list">' +products.map(product=>createPlanHtml(product,commonFeatures)).join('') +'</ul> <button class="right scrollbutton" onclick="rightScroll()">&gt;</button></div>';
